6385915ade8409096552da96cbd2469da1501be2
mandalmrinmoy <112158728+mandalmrinmoy@users.noreply.github.com>
2024-05-26 00:30:26 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,21 @@
+chai="Masala Chai"
+print(chai)
+print(chai.lower())
+print(chai.upper())
+
+# Slice
+num="0123456789"
+print(num[:])
+print(num[3:])
+print(num[:5])
+print(num[:-5])
+print(num[1:-2])
+
+name="Mrinmoy"
+print(name.replace("Mr","Ch"))
+
+name1="She said that \"You are cute\""
+print(name1)
+
+name2=r"Hjgfhtfdj\kjfhjj";
+print(name2)
